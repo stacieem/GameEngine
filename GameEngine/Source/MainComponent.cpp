10b8823889c2b4065aee9b1847eb7bd0e126f8a8
Tim Arterbury <timarterbury@gmail.com>
2017-08-26 14:32:58 -0500

    Super basic JUCE project example.

---
+++ GameEngine/Source/MainComponent.cpp
@@ -0,0 +1,55 @@
+/*
+  ==============================================================================
+
+    This file was auto-generated!
+
+  ==============================================================================
+*/
+
+#include "MainComponent.h"
+
+
+//==============================================================================
+MainContentComponent::MainContentComponent()
+{
+    button.setButtonText ("Lolz wutt");
+    addAndMakeVisible (button);
+    
+    backgroundColour = Colours::black;
+    button.addListener (this);
+    
+    setSize (600, 400);
+}
+
+MainContentComponent::~MainContentComponent()
+{
+}
+
+void MainContentComponent::paint (Graphics& g)
+{
+    
+    // (Our component is opaque, so we must completely fill the background with a solid colour)
+    //g.fillAll (getLookAndFeel().findColour (ResizableWindow::backgroundColourId));
+    g.fillAll (backgroundColour);
+
+    //g.setFont (Font (16.0f));
+    //g.setColour (Colours::white);
+    //g.drawText ("Hello World!", getLocalBounds(), Justification::centred, true);
+}
+
+void MainContentComponent::resized()
+{
+    // This is called when the MainContentComponent is resized.
+    // If you add any child components, this is where you should
+    // update their positions.
+    button.setBounds (30, 30, 200, 200);
+}
+
+void MainContentComponent::buttonClicked (Button * buttonPtr)
+{
+    if (buttonPtr == &button)
+    {
+        backgroundColour = Colours::white;
+        repaint();
+    }
+}
---
+++ GameEngine/Source/MainComponent.h
@@ -0,0 +1,36 @@
+/*
+  ==============================================================================
+
+    This file was auto-generated!
+
+  ==============================================================================
+*/
+
+#pragma once
+
+#include "../JuceLibraryCode/JuceHeader.h"
+
+
+//==============================================================================
+/*
+    This component lives inside our window, and this is where you should put all
+    your controls and content.
+*/
+class MainContentComponent   : public Component, public Button::Listener
+{
+public:
+    //==============================================================================
+    MainContentComponent();
+    ~MainContentComponent();
+
+    void paint (Graphics&) override;
+    void resized() override;
+    void buttonClicked (Button *) override;
+
+private:
+    //==============================================================================
+    TextButton button;
+    Colour backgroundColour;
+    
+    JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR (MainContentComponent)
+};
