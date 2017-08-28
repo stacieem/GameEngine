c4f86e1783cf395f3538e0f5b45e3efa791fddec
Tim Arterbury <timarterbury@gmail.com>
2017-08-28 17:02:49 -0500

    Sets up shaders in GameView

--- GameEngine/Source/GameView.h
+++ GameEngine/Source/GameView.h
@@ -22,17 +22,32 @@ class GameView :    public Component,
 public:
     GameView()
     {
+        // Sets the OpenGL version to 3.2
+        // This is very important, if this is not included, new shader syntax
+        // will cause a compiler error.
+        openGLContext.setOpenGLVersionRequired (OpenGLContext::OpenGLVersion::openGL3_2);
+        
         openGLContext.setRenderer(this);
         openGLContext.attachTo(*this);
         
         addAndMakeVisible(gameHUD);
         
+        // Setup GUI Overlay Label: Status of Shaders, compiler errors, etc.
+        addAndMakeVisible (statusLabel);
+        statusLabel.setJustificationType (Justification::topLeft);
+        statusLabel.setFont (Font (14.0f));
+        statusLabel.toBack();
+        
+        // GameView Variables
         isEnabled = false;
+        
         setOpaque(true);
     }
     
     ~GameView()
     {
+        // Turn off OpenGL
+        openGLContext.setContinuousRepainting (false);
         openGLContext.detach();
     }
     
@@ -58,19 +73,81 @@ public:
     // OpenGL Callbacks ========================================================
     void newOpenGLContextCreated() override
     {
+        // Setup Shaders
+        createShaders();
         
+        // Setup Buffer Objects
+        openGLContext.extensions.glGenBuffers (1, &VBO); // Vertex Buffer Object
+        openGLContext.extensions.glGenBuffers (1, &EBO); // Element Buffer Object
     }
     
     void openGLContextClosing() override
     {
-        
+        shader = nullptr;
+        uniforms = nullptr;
     }
     
     void renderOpenGL() override
     {
         jassert (OpenGLHelpers::isContextActive());
         
-        OpenGLHelpers::clear (Colours::red);
+        // Setup Viewport
+        const float renderingScale = (float) openGLContext.getRenderingScale();
+        glViewport (0, 0, roundToInt (renderingScale * getWidth()), roundToInt (renderingScale * getHeight()));
+    
+        // Set background Color
+        OpenGLHelpers::clear (getLookAndFeel().findColour (ResizableWindow::backgroundColourId));
+        
+        // Enable Alpha Blending
+        glEnable (GL_BLEND);
+        glBlendFunc (GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
+        
+        // Use Shader Program that's been defined
+        shader->use();
+
+        // Define Vertices for a Square (the view plane)
+        GLfloat vertices[] = {
+            1.0f,   1.0f,  0.0f,  // Top Right
+            1.0f,  -1.0f,  0.0f,  // Bottom Right
+            -1.0f, -1.0f,  0.0f,  // Bottom Left
+            -1.0f,  1.0f,  0.0f   // Top Left
+        };
+        // Define Which Vertex Indexes Make the Square
+        GLuint indices[] = {  // Note that we start from 0!
+            0, 1, 3,   // First Triangle
+            1, 2, 3    // Second Triangle
+        };
+
+        // VBO (Vertex Buffer Object) - Bind and Write to Buffer
+        openGLContext.extensions.glBindBuffer (GL_ARRAY_BUFFER, VBO);
+        openGLContext.extensions.glBufferData (GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STREAM_DRAW);
+                                                                                        // GL_DYNAMIC_DRAW or GL_STREAM_DRAW
+                                                                                        // We may want GL_DYNAMIC_DRAW since it is for
+                                                                                        // vertex data that will be changing alot.
+        
+        // EBO (Element Buffer Object) - Bind and Write to Buffer
+        openGLContext.extensions.glBindBuffer (GL_ELEMENT_ARRAY_BUFFER, EBO);
+        openGLContext.extensions.glBufferData (GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STREAM_DRAW);
+                                                                                // GL_DYNAMIC_DRAW or GL_STREAM_DRAW
+                                                                                // We may want GL_DYNAMIC_DRAW since it is for
+                                                                                // vertex data that will be changing alot.
+       
+        // Setup Vertex Attributes
+        openGLContext.extensions.glVertexAttribPointer (0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(GLfloat), (GLvoid*)0);
+        openGLContext.extensions.glEnableVertexAttribArray (0);
+        
+        // Draw Vertices
+        glDrawElements (GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0); // For EBO's (Element Buffer Objects) (Indices)
+        //glDrawArrays (GL_TRIANGLES, 0, 6); // For just VBO's (Vertex Buffer Objects)
+        
+        
+        // Reset the element buffers so child Components draw correctly
+        openGLContext.extensions.glBindBuffer (GL_ARRAY_BUFFER, 0);
+        openGLContext.extensions.glBindBuffer (GL_ELEMENT_ARRAY_BUFFER, 0);
+        //openGLContext.extensions.glBindVertexArray(0);
+
+
+
     }
     
     
@@ -78,6 +155,7 @@ public:
     void resized() override
     {
         gameHUD.setBounds(getLocalBounds());
+        statusLabel.setBounds (getLocalBounds().reduced (4).removeFromTop (75));
     }
     
     void paint(Graphics & g) override
@@ -86,13 +164,121 @@ public:
     
 private:
     
+    //==========================================================================
+    // OpenGL Shader Functions
+
+    /** Calculates and returns the Projection Matrix.
+     */
+//    Matrix3D<float> getProjectionMatrix() const
+//    {
+//        float w = 1.0f / (0.5f + 0.1f);
+//        float h = w * getLocalBounds().toFloat().getAspectRatio (false);
+//        return Matrix3D<float>::fromFrustum (-w, w, -h, h, 4.0f, 30.0f);
+//    }
+    
+    /** Calculates and returns the View Matrix.
+     */
+//    Matrix3D<float> getViewMatrix() const
+//    {
+//        Matrix3D<float> viewMatrix (Vector3D<float> (0.0f, 0.0f, -10.0f));
+//        Matrix3D<float> rotationMatrix = draggableOrientation.getRotationMatrix();
+//        
+//        return rotationMatrix * viewMatrix;
+//    }
+    
+    /** Loads the OpenGL Shaders and sets up the whole ShaderProgram
+     */
+    void createShaders()
+    {
+        vertexShader =
+        "#version 330 core\n"
+        "layout (location = 0) in vec2 position;\n"
+        "\n"
+        "void main()\n"
+        "{\n"
+        "    gl_Position = vec4(position, 0.0f, 1.0f);\n"
+        "}\n";
+        
+        // Base Fragment-Shader paints the object green.
+        fragmentShader =
+        "#version 330 core\n"
+        "out vec4 color;\n"
+        "void main()\n"
+        "{\n"
+        "    color = vec4 (0.0f, 1.0f, 0.0f, 1.0f);\n"
+        "}\n";
+        
+        
+        ScopedPointer<OpenGLShaderProgram> newShader (new OpenGLShaderProgram (openGLContext));
+        String statusText;
+        
+        if (newShader->addVertexShader ((vertexShader))
+            && newShader->addFragmentShader ((fragmentShader))
+            && newShader->link())
+        {
+            //attributes = nullptr;
+            uniforms = nullptr;
+            
+            shader = newShader;
+            shader->use();
+            
+            uniforms   = new Uniforms (openGLContext, *shader);
+            
+            statusText = "GLSL: v" + String (OpenGLShaderProgram::getLanguageVersion(), 2);
+        }
+        else
+        {
+            statusText = newShader->getLastError();
+        }
+        
+        statusLabel.setText (statusText, dontSendNotification);
+    }
+    
+    //==============================================================================
+    // This class manages the uniform values that the shaders use.
+    struct Uniforms
+    {
+        Uniforms (OpenGLContext& openGLContext, OpenGLShaderProgram& shaderProgram)
+        {
+            projectionMatrix = createUniform (openGLContext, shaderProgram, "projectionMatrix");
+            viewMatrix       = createUniform (openGLContext, shaderProgram, "viewMatrix");
+        }
+        
+        ScopedPointer<OpenGLShaderProgram::Uniform> projectionMatrix, viewMatrix;
+        //ScopedPointer<OpenGLShaderProgram::Uniform> lightPosition;
+        
+        private:
+        static OpenGLShaderProgram::Uniform* createUniform (OpenGLContext& openGLContext,
+                                                            OpenGLShaderProgram& shaderProgram,
+                                                            const char* uniformName)
+        {
+            if (openGLContext.extensions.glGetUniformLocation (shaderProgram.getProgramID(), uniformName) < 0)
+            return nullptr;
+            
+            return new OpenGLShaderProgram::Uniform (shaderProgram, uniformName);
+        }
+    };
+    
+    
     bool isEnabled;
     
     // OpenGL Rendering
     OpenGLContext openGLContext;
-    ScopedPointer<OpenGLShaderProgram> shader; // When we use shaders to make crap look dope
+    ScopedPointer<OpenGLShaderProgram> shader;
+    ScopedPointer<Uniforms> uniforms;
+    GLuint VBO;
+    GLuint VAO;
+    GLuint EBO;
+    //GLuint xzVBO;
+    //GLuint yVBO;
     
-    // Components
+    const char* vertexShader;
+    const char* fragmentShader;
+    
+    // JUCE Components
     GameHUD gameHUD;
+    
+    // DEBUGGING
+    Label statusLabel;
     
 };
