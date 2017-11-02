fda2d226950406b2d5d19ddaad2184a5944dc67c
Morgan Wesemann <morgan_wesemann@baylor.edu>
2017-11-02 15:36:03 -0500

    Left animation works again

--- GameEngine/Source/GameView.h
+++ GameEngine/Source/GameView.h
@@ -201,11 +201,11 @@ public:
 			// Reverse texture coords if left animation
 			if (renderableObject.animationProperties.isLeftAnimation())
 			{
-				//uniforms->isLeftAnimation->set(true);
+				uniforms->isLeftAnimation->set(true);
 			}
 			else
 			{
-				//uniforms->isLeftAnimation->set(false);
+				uniforms->isLeftAnimation->set(false);
 			
 			}
             
@@ -346,35 +346,40 @@ private:
      */
     void createShaders()
     {
-        vertexShader =
-//        "#version 330 core\n"
-//        "layout (location = 0) in vec2 position;\n"
-//		"layout (location = 1) in vec2 textureCoordIn;\n"
-//        "uniform mat4 projectionMatrix;\n"
-//        "uniform mat4 viewMatrix;\n"
-//		"out vec2 textureCoordOut;\n"
-//        "\n"
-//        "void main()\n"
-//        "{\n"
-//		"    textureCoordOut = textureCoordIn;\n"
-//        "    gl_Position = projectionMatrix * viewMatrix * vec4(position, 0.0f, 1.0f);\n"
-//        "}\n";
-        "#version 330 core\n"
-        "layout (location = 0) in vec3 position;\n"
-        "layout (location = 1) in vec4 color;\n"
-        "layout (location = 2) in vec2 textureCoordIn;\n"
-        "uniform mat4 projectionMatrix;\n"
-        "uniform mat4 viewMatrix;\n"
-        "uniform mat4 modelMatrix;\n"
-        "out vec4 vertexColor;\n"
-        "out vec2 textureCoordOut;\n"
-        "\n"
-        "void main()\n"
-        "{\n"
-        "    textureCoordOut = textureCoordIn;\n"
-        "    vertexColor = color;\n"
-        "    gl_Position = projectionMatrix * viewMatrix * modelMatrix * vec4(position, 1.0f);\n"
-        "}\n";
+		vertexShader =
+			//        "#version 330 core\n"
+			//        "layout (location = 0) in vec2 position;\n"
+			//		"layout (location = 1) in vec2 textureCoordIn;\n"
+			//        "uniform mat4 projectionMatrix;\n"
+			//        "uniform mat4 viewMatrix;\n"
+			//		"out vec2 textureCoordOut;\n"
+			//        "\n"
+			//        "void main()\n"
+			//        "{\n"
+			//		"    textureCoordOut = textureCoordIn;\n"
+			//        "    gl_Position = projectionMatrix * viewMatrix * vec4(position, 0.0f, 1.0f);\n"
+			//        "}\n";
+			"#version 330 core\n"
+			"layout (location = 0) in vec3 position;\n"
+			"layout (location = 1) in vec4 color;\n"
+			"layout (location = 2) in vec2 textureCoordIn;\n"
+			"uniform mat4 projectionMatrix;\n"
+			"uniform mat4 viewMatrix;\n"
+			"uniform mat4 modelMatrix;\n"
+			"uniform bool isLeftAnimation;\n"
+			"out vec4 vertexColor;\n"
+			"out vec2 textureCoordOut;\n"
+			"\n"
+			"void main()\n"
+			"{\n"
+			"	if(isLeftAnimation == true) {"
+			"		textureCoordOut = vec2(1.0 - textureCoordIn.s, textureCoordIn.t);\n"
+			"   } else {\n"
+			"		textureCoordOut = textureCoordIn;\n"
+			"	}\n"
+			"    vertexColor = color;\n"
+			"    gl_Position = projectionMatrix * viewMatrix * modelMatrix * vec4(position, 1.0f);\n"
+			"}\n";
         
         
         fragmentShader =
