d2373bbab233c8aa90e4c4135ac7714f44358c27
Tim Arterbury <timarterbury@gmail.com>
2017-11-09 16:26:42 -0600

    More efficient Gridding solution.

--- GameEngine/Source/WorldGrid.h
+++ GameEngine/Source/WorldGrid.h
@@ -30,42 +30,16 @@ public:
     
     glm::vec2 getGriddedPostition (glm::vec2 ungriddedPosition)
     {
-        glm::vec2 griddedPosition;
-        
-        // Get the number of unit lengths found in the ungridded position
-        glm::vec2 unitLengthsInUngridded = ungriddedPosition / unitLength;
-        
-        // If the decimal values of the x value of number of unit lengths is rounded
-        // to zero
-        if (round (abs(unitLengthsInUngridded.x) - floor (abs(unitLengthsInUngridded.x))) == 0)
-        {
-            griddedPosition.x = unitLength * trunc(unitLengthsInUngridded.x);
-        }
-        else
-        {
-            if (unitLengthsInUngridded.x >= 0.0f)
-                griddedPosition.x = unitLength * (1 + trunc(unitLengthsInUngridded.x));
-            else
-                griddedPosition.x = unitLength * (-1 + trunc(unitLengthsInUngridded.x));
-        }
-        
-        // If the decimal values of the y value of number of unit lengths is rounded
-        // to zero
-        if (round (abs(unitLengthsInUngridded.y) - floor (abs(unitLengthsInUngridded.y))) == 0)
-        {
-            griddedPosition.y = unitLength * trunc(unitLengthsInUngridded.y);
-        }
-        else
-        {
-            if (unitLengthsInUngridded.y >= 0.0f)
-                griddedPosition.y = unitLength * (1 + trunc(unitLengthsInUngridded.y));
-            else
-                griddedPosition.y = unitLength * (-1 + trunc(unitLengthsInUngridded.y));
-        }
-        
-        return griddedPosition;
+        return glm::vec2(roundToClosestGridUnit(ungriddedPosition.x),
+                         roundToClosestGridUnit(ungriddedPosition.y));
     }
     
 private:
+    
+    float roundToClosestGridUnit (float distance)
+    {
+        return round(distance / unitLength) * unitLength;
+    }
+    
     float unitLength;
 };
